a25dcfb3d68deb1ef523727165adce1f922ffc41
Teddykavooh <teddykavooh@gmail.com>
2022-08-08 17:47:57 +0300

    Add footer Add slideshow functionality

--- myScript.js
+++ myScript.js
@@ -1,4 +1,4 @@
-// Slideshow
+// Slideshow Auto
 let slideIndex = 0;
 showSlides();
 
@@ -17,4 +17,32 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+//Slideshow
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides(n) {
+  showSlides1(slideIndex1 += n);
+}
+
+function currentSlide(n) {
+  showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex1 = 1}    
+  if (n < 1) {slideIndex1 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex1-1].style.display = "block";  
+  dots[slideIndex1-1].className += " active";
 }
