d5a97a0c9557f979455d19820f660c1f3e243702
Teddykavooh <teddykavooh@gmail.com>
2022-12-14 15:30:25 +0300

    Tweak the js script file

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// Tabbed component
+/*Tabbed component*/
 
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
